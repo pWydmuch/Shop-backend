b2bb3c4e5338f83b94f6f7133a8c226b2e07f045
Patryk Wydmuch <pvydmuch@gmail.com>
2020-03-16 20:42:40 +0100

    Change cors

--- src/main/java/wydmuch/patryk/psw2/controllers/ProductController.java
+++ src/main/java/wydmuch/patryk/psw2/controllers/ProductController.java
@@ -8,7 +8,7 @@ import wydmuch.patryk.psw2.repositories.ProductRepository;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://my-shop-123.surge.sh/*", allowCredentials = "true")
+@CrossOrigin(origins = "http://my-shop-123.surge.sh", allowCredentials = "true")
 @RestController
 public class ProductController {
 
